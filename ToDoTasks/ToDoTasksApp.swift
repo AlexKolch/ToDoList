e5d7df8f6ec23454fb14b12ab2360e088fb0d613
Alex Kolch <alexkolch.ios@gmail.com>
2025-01-27 12:16:28 +0300

    Initial Commit

---
+++ ToDoTasks/ToDoTasksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoTasksApp.swift
+//  ToDoTasks
+//
+//  Created by Алексей Колыченков on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoTasksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
